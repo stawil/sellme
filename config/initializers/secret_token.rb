b2da414546452d6d853ce6fffc8bb5028dce98bc
stawil <stawil@webaftweweb.com>
2012-04-24 18:09:09 +0100

    starter app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sellme::Application.config.secret_token = 'efb4645fc3c4b4d3417784932eb50328e322df961507eb3a2e7027b5c0e68e21e9705212fc2d5916c2ee79e3e64dcf438034abba47504af6a19f26936c1b5ddb'
